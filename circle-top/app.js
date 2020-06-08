de42bdafeb08b608da9d454f0fc8668c279d4f26
Taiga <taiga248@gmail.com>
2020-06-08 09:40:42 +0900

    Change name circle-top => header_anima....

--- circle-top/app.js
+++
@@ -1,5 +0,0 @@
-const section = document.querySelector("section")
-window.addEventListener("scroll", () => {
-    const value = window.scrollY;
-    section.style.clipPath = "circle("+ value +"px at center)"; 
-})
